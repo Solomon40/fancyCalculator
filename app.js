218b077dac69e553c1dba02df76a4337de4cb87b
Tochukwu Ewiwilem <tochukwuewiwilem@gmail.com>
2022-09-05 01:57:59 +0200

    implement compute

--- app.js
+++ app.js
@@ -2,13 +2,74 @@ class Calculator {
     constructor (prevOperandTextElement, postOperandTextElement) {
         this.prevOperandTextElement = prevOperandTextElement;
         this.postOperandTextElement = postOperandTextElement;
-        this.clear(d)
+        this.clear()
     }
-// clear the display (output) on click of allClearButton
+
     clear(){
+        this.prevOperand = "";
+        this.postOperand = "";
+        this.operation = undefined;
+    }
+
+    delete(){
 
     }
+
+    appendNumber (number){
+        if (number === "." && this.postOperand.includes(".")) return                    // check incoming fullstop against existing number to avoid duplicate points
+        this.postOperand = this.postOperand.toString() + number.toString();             
+        
+    }
+
+    chooseOperation (operation){
+        if (this.postOperand === '') {                                                  //Check if previous operand exists and update operator rather than entire previous operand output
+            
+            this.prevOperand = this.prevOperand.slice(0, -1) + operation;
+            return;
+        }
+        if (this.prevOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.prevOperand = this.postOperand + operation;                                
+        this.postOperand = "";                                                          
+    }
+
+    compute(){
+        let computation;
+        const prev = parseFloat(this.prevOperand);
+        const post = parseFloat(this.postOperand);
+
+        switch (this.operation) {
+            case ('+'):
+                computation = prev + post;
+                break;
+            case ('-'):
+                computation = prev - post;
+                break;
+            case ('/'):
+                computation = prev / post;
+                break;
+            case ('*'):
+                computation = prev * post;
+                break;
+            default:
+                return;
+        }
+
+        this.postOperand = computation;
+        this.operation = undefined;
+        this.prevOperand = "";
+    }
+
+    updateDisplay(){
+        this.postOperandTextElement.innerText = this.postOperand;
+        this.prevOperandTextElement.innerText = this.prevOperand;
+    }
 }
+
+
+
 
 const numberButton = document.querySelectorAll('[data-number]');
 const operatorButton = document.querySelectorAll('[data-operator]');
@@ -17,4 +78,43 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 
 const prevOperandTextElement = document.querySelector('[data-prev-operand]');
-const postOperandTextElement = document.querySelector('[data-post-operand]');
+const postOperandTextElement = document.querySelector('[data-post-operand]'); 
+
+//instantiate the Calculator instantiate the calculator class 
+const calculator = new Calculator(prevOperandTextElement, postOperandTextElement);
+
+numberButton.forEach(
+    button => {
+        button.addEventListener('click', () => {
+            calculator.appendNumber(button.innerText);
+            calculator.updateDisplay();
+        })
+    }
+)
+
+allClearButton.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+    }
+)
+
+operatorButton.forEach(
+    button => {
+        button.addEventListener('click', () => {
+            calculator.chooseOperation(button.innerText);
+            calculator.updateDisplay();
+        })
+    }
+)
+
+equalButton.addEventListener('click', () => {
+    calculator.compute();
+    calculator.updateDisplay();
+    }
+)
+
+
+
+//
+//
+//
